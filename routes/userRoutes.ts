3ea3324433245a3fd2a9acfa5128905b7df92b0f
ibrahim <74019633+ibrahemaskar11@users.noreply.github.com>
2024-04-25 15:39:42 +0200

    friendships

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,15 +1,24 @@
-
 import { Router } from "express";
 import {
   forgotPassword,
   login,
   logout,
+  protect,
   resetPassword,
   signup,
   validate,
 } from "../controllers/authController";
 
-import { getAllUsers, getOneUser,updateUser,deleteUser,search } from "../controllers/userController";
+import {
+  getAllUsers,
+  getOneUser,
+  updateUser,
+  deleteUser,
+  searchUsers,
+  createFriendshipRequest,
+  acceptFriendRequest,
+  rejectFriendRequest,
+} from "../controllers/userController";
 
 const router: Router = Router();
 
@@ -18,10 +27,17 @@ router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 router.get("/validate", validate);
+router.use(protect)
 router.get("/logout", logout);
-router.get("/", getAllUsers);
-router.get("/:id", getOneUser).patch("/:id", updateUser).delete("/:id", deleteUser);
-router.post("/search", search);
-
+router.get("/search/:searchTerm", searchUsers);
+// router.get("/", getAllUsers);
+// router
+//   .get("/:id", getOneUser)
+//   .patch("/:id", updateUser)
+//   .delete("/:id", deleteUser);
+  
+router.post("/friendship/request", createFriendshipRequest);
+router.put("/friendship/accept", acceptFriendRequest);
+router.put("/friendship/reject", rejectFriendRequest);
 
 export default router;
